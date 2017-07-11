99e1b327d5ee6d9e48bdf4c9a5b5c0d9ec338142
Gennady Verdel <godrose@gmail.com>
2017-07-11 10:01:31 +0300

    Added net46 project to test netstandard1.3 library

---
+++ src/LogoFX.Core.NETStandard13.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LogoFX.Core.NETStandard13.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogoFX.Core.NETStandard13.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6825a144-1a07-4e95-b90d-25fd37924707")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
