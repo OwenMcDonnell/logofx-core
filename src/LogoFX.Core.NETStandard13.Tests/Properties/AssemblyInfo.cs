11022bf150381bfeab4d26a7520581ce45c94d56
Gennady Verdel <godrose@gmail.com>
2017-07-14 19:53:11 +0300

    Replaced net standard 1.3 project

--- src/LogoFX.Core.NETStandard13.Tests/Properties/AssemblyInfo.cs
+++ src/LogoFX.Core.NETStandard13.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("LogoFX.Core.NETStandard13.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -10,10 +13,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: Guid("6825a144-1a07-4e95-b90d-25fd37924707")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25260c17-a35c-4fe6-8143-001e4460664b")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
