cb1c99b29734627695f0c57e6f09ceaab0db3daf
Gennady Verdel <godrose@gmail.com>
2018-11-17 15:27:09 +0200

    Fixed duplicate file issue

--- src/LogoFX.Core.Tests/Properties/AssemblyInfo.cs
+++ src/LogoFX.Core.Tests/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LogoFX.Core.NETStandard20.Tests")]
+[assembly: AssemblyTitle("LogoFX.Core.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LogoFX.Core.NETStandard20.Tests")]
+[assembly: AssemblyProduct("LogoFX.Core.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
